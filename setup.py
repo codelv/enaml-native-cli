e277fcf41cf39794302438190bde3153bb811df9
frmdstryr <frmdstryr@gmail.com>
2017-10-31 20:10:09 -0400

    Update version number

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_data_files(dest, *folders):
 
 setup(
     name="enaml-native-cli",
-    version="1.0",
+    version="1.1",
     author="CodeLV",
     author_email="frmdstryr@gmail.com",
     license='MIT',
