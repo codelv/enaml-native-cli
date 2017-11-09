8c1726c06b1721af8bbddc59756a8861153a4272
frmdstryr <frmdstryr@gmail.com>
2017-11-09 09:35:10 -0500

    Try fixing travis again

--- python-for-android/p4a.py
+++ python-for-android/p4a.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
+
+if 'TRAVIS' in os.environ:
+    #: Force UTF8 encoding
+    import sys
+    # sys.setdefaultencoding() does not exist, here!
+    reload(sys)  # Reload does the trick!
+    sys.setdefaultencoding('UTF8')
+
 from pythonforandroid.toolchain import main
 
 if __name__=='__main__':
