eba834f88aca8842302f16b3877a5dd5a67d4bad
frmdstryr <frmdstryr@gmail.com>
2017-12-30 11:38:53 -0500

    Update to newer code formart used and add debugging hook

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_data_files(dest, *folders):
 
 setup(
     name="enaml-native-cli",
-    version="1.5.0",
+    version="1.5.1",
     author="CodeLV",
     author_email="frmdstryr@gmail.com",
     license='MIT',
@@ -58,7 +58,7 @@ setup(
                                'python-for-android', 'python-for-ios'),
     install_requires=[
         'appdirs', 'colorama>=0.3.3', 'sh==1.12.4', 'jinja2', 'six',
-        'pipdeptree', 'atom', 'ply',
+        'pipdeptree', 'atom', 'ply', 'enaml-native>=2.13.2'
     ],
     setup_requires=['virtualenv'],
     test_requires=['requests', 'py.test', 'pytest-cov', 'pytest-catchlog',
