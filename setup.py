d5880fb166e043c9bb418c8d979dc719648b7af4
frmdstryr <frmdstryr@gmail.com>
2018-01-05 09:33:55 -0500

    Don't reload unless view exists

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_data_files(dest, *folders):
 
 setup(
     name="enaml-native-cli",
-    version="1.5.1",
+    version="1.5.2",
     author="CodeLV",
     author_email="frmdstryr@gmail.com",
     license='MIT',
