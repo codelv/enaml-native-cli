12b29a8280889edd8c3edae273e16ea2402fc689
frmdstryr <frmdstryr@gmail.com>
2018-03-06 13:16:31 -0500

    Bump version

--- enamlnativecli/templates/app/hooks/post_gen_project.py
+++ enamlnativecli/templates/app/hooks/post_gen_project.py
@@ -8,6 +8,5 @@ def process(line):
 
 
 p = sh.conda('create', '-p', 'venv', '-c', 'codelv', '--use-local', '--yes',
-             'enaml-native',
-             _bg=True, _err_to_out=True, _out=process, _out_buffer=1)
+             'enaml-native', _bg=True, _err_to_out=True, _out=process)
 p.wait()
