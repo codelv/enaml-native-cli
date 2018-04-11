ac47e983989e4a67da6d21ca264799c3aad0ccc9
frmdstryr <frmdstryr@gmail.com>
2018-04-10 23:51:50 -0400

    Add conda recipe and show icon for default layout

--- enamlnativecli/templates/app/{{cookiecutter.project_name}}/android/app/src/main/java/{{cookiecutter.bundle_path}}/MainActivity.java
+++ enamlnativecli/templates/app/{{cookiecutter.project_name}}/android/app/src/main/java/{{cookiecutter.bundle_path}}/MainActivity.java
@@ -1,6 +1,17 @@
 package {{cookiecutter.bundle_id}};
 
+import android.view.View;
+import android.widget.TextView;
+
 import com.codelv.enamlnative.EnamlActivity;
 
 public class MainActivity extends EnamlActivity {
+
+    public View getLoadingScreen() {
+        return findViewById(R.id.loadingView);
+    }
+
+    public TextView getMessageTextView() {
+        return findViewById(R.id.textView);
+    }
 }
