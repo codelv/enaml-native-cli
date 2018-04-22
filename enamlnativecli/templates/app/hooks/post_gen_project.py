02a7c16622c5cfed005833dc9d1a93f5fa3d7723
frmdstryr <frmdstryr@gmail.com>
2018-04-22 01:12:26 -0400

    Bump version

--- enamlnativecli/templates/app/hooks/post_gen_project.py
+++ enamlnativecli/templates/app/hooks/post_gen_project.py
@@ -9,5 +9,6 @@ def process(line):
 
 
 p = sh.conda('create', '-p', 'venv', '-c', 'codelv', '--use-local', '--yes',
+             'python={{cookiecutter.python_version}}',
              'enaml-native', _bg=True, _err_to_out=True, _out=process)
 p.wait()
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def find_data(folder):
 
 setup(
     name="enaml-native-cli",
-    version="2.2.2",
+    version="2.2.3",
     author="CodeLV",
     author_email="frmdstryr@gmail.com",
     license='GPLv3',
